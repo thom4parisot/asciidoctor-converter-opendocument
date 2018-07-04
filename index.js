cef43d73e5eaaa8dbd6834ecbd96bb03a95aca3b
Thomas Parisot <oncletom@users.noreply.github.com>
2018-07-04 19:38:59 +0200

    Expose XML document and convert it on our own

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const simpleOdf = require('simple-odf');
+const {DOMImplementation,XMLSerializer, DOMParser} = require('xmldom');
 
-module.exports = function ConverterODT(Asciidoctor) {
+const XML_DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>';
+
+module.exports = function ConverterODT(Asciidoctor, {withXml}) {
   class ODTConverter {
     constructor(backend, opts) {
       this.basebackend = 'xml';
@@ -14,11 +17,26 @@ module.exports = function ConverterODT(Asciidoctor) {
 
     $convert(node, transform = null, opts = {}) {
       const {_doc: doc} = this;
+      let document, root;
 
       switch (transform || node.node_name) {
         case 'document':
           node.getContent();
-          return String(doc);
+          document = new DOMImplementation().createDocument(
+            'urn:oasis:names:tc:opendocument:xmlns:office:1.0',
+            'office:document',
+            null);
+          root = document.firstChild;
+
+          doc.toXml(document, root);
+
+          if (typeof withXml === 'function') {
+            withXml(document, DOMParser);
+          }
+
+          return `${XML_DECLARATION}
+
+${new XMLSerializer().serializeToString(document).replace(/&amp;([^\s&])/g, '&$1')}`;
 
         case 'section':
           doc.addHeading(node.getTitle(), node.getLevel());
