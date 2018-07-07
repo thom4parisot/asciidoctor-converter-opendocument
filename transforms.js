50484f9d5b769904aaa1d1e0ece094e05f83fbe3
Thomas Parisot <oncletom@users.noreply.github.com>
2018-07-07 23:49:01 +0200

    Added a bunch of tags

--- transforms.js
+++ transforms.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const XML_DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>';
-
 module.exports = {
   document: ({node, styles}) => {
     return `<?xml version="1.0" encoding="UTF-8"?>
@@ -9,20 +7,20 @@ module.exports = {
   <office:meta>
     <meta:generator>asciidoctor-converter-opendocument</meta:generator>
     <dc:title>${node.getDocument().getDoctitle()}</dc:title>
-    <meta:creation-date>${node.getDocument().getRevisionDate()}</meta:creation-date>
+    <meta:creation-date>${node.getDocument().getRevdate()}</meta:creation-date>
     <dc:language>${node.getDocument().getAttribute('lang')}</dc:language>
     <meta:editing-cycles>1</meta:editing-cycles>
     <meta:editing-duration>P0D</meta:editing-duration>
-    <meta:document-statistic meta:character-count="10290" meta:image-count="0" meta:non-whitespace-character-count="8743" meta:object-count="0" meta:page-count="7" meta:paragraph-count="143" meta:table-count="1" meta:word-count="1658"/>
+    <meta:document-statistic meta:character-count="0" meta:image-count="0" meta:non-whitespace-character-count="0" meta:object-count="0" meta:page-count="0" meta:paragraph-count="0" meta:table-count="0" meta:word-count="0"/>
     <meta:user-defined meta:name="Info 1"/>
     <meta:user-defined meta:name="Info 2"/>
     <meta:user-defined meta:name="Info 3"/>
     <meta:user-defined meta:name="Info 4"/>
   </office:meta>
-  ${styles.settings}
-  ${styles.scripts}
-  ${styles.fonts}
-  ${styles.styles}
+  ${styles.settings || ''}
+  ${styles.scripts || ''}
+  ${styles.fonts || ''}
+  ${styles.styles || ''}
   <office:body>
     <office:text>${node.getContent()}</office:text>
   </office:body>
@@ -30,26 +28,33 @@ module.exports = {
   },
 
   section: ({node}) => {
-    const style = node.getLevel() === 1 ? 'ChapitreTitre': `TitreNiveau${node.getLevel()-1}`;
-    return `<text:h text:style-name="${style}" text:outline-level="${node.getLevel()}">${node.getTitle()}</text:h>${node.getContent()}`;
-    // return ['#'.repeat(node.getLevel()) + ' ' + node.getTitle(), node.getContent()].join('\n\n');
+    const style = node.getLevel() === 1 ? 'ChapitreTitre' : `TitreNiveau${node.getLevel()-1}`;
+    let pre = '';
+
+    if (node.getLevel() === 1 && node.getDocument().hasAttribute('chapternumber')) {
+      pre = `<text:p text:style-name="ChapitreNumero">${node.getDocument().getAttribute('chapternumber')}</text:p>`;
+    }
+
+    return `${pre}<text:h text:style-name="${style}" text:outline-level="${node.getLevel()}">${node.getTitle()}</text:h>${node.getContent()}`;
   },
   paragraph: ({node}) => {
     const style = node.getParent().node_name === 'admonition' ? 'Remarque' : 'TexteCourant';
 
-    return `<text:p text:style-name="${style}">${node.getContent().trim()}</text:p>`;
+    return `<text:p text:style-name="${style}">${node.getContent().replace(/(<text:line-break\/>)\n/gm, '$1')}</text:p>`;
   },
 
   inline_quoted: ({node}) => {
     let pre = '';
     let post = '';
+    let styleName = node.getRole() || node.getType();
 
     if (node.getRole() === 'bare') {
       pre = '<![CDATA[';
       post = ']]>';
+      styleName = 'CodeDansTexte';
     }
 
-    return `<text:span text:style-name="${node.getRole() || node.getType()}">${pre}${node.getText()}${post}</text:span>`;
+    return `<text:span text:style-name="${styleName}">${pre}${node.getText()}${post}</text:span>`;
   },
 
   inline_anchor: ({node}) => {
@@ -65,7 +70,7 @@ module.exports = {
   },
 
   inline_break: ({node}) => {
-    return `${node.getText().trim()}<text:line-break/>`;
+    return `${node.getText()}<text:line-break/>`;
   },
 
   inline_kbd: ({node}) => {
@@ -82,14 +87,17 @@ module.exports = {
     if (node.getTitle()) {
       title = `<text:p text:style-name="CodeTitre">${node.getTitle()}</text:p>`;
     }
-    const code = node.getSourceLines().map(line => `<text:p text:style-name="Code"><![CDATA[${line}]]></text:p>`).join('');
+
+    const code = node.getContent().split('\n').map(line => {
+      return `<text:p text:style-name="Code">${line.replace(/&&/g, '&amp;&amp;').replace(/(^\s+|\s+(<text:span))/g, (m, chars, after) => `<text:s text:c="${chars.length}"/>${after || ''}`)}</text:p>`;
+    }).join('');
 
     return `${title}${code}`;
   },
 
   ulist: ({node}) => {
     const items = node.getItems().map(item => `<text:list-item>
-  <text:p text:style-name="ListeAPuce">${item.getText()}</text:p>
+<text:p text:style-name="ListeAPuce">${item.getText()}</text:p>
 </text:list-item>`).join('');
 
     return `<text:list text:style-name="List_20_1">${items}</text:list>`;
@@ -100,19 +108,39 @@ module.exports = {
   },
 
   dlist: ({node}) => {
-    return '';
+    return node.getItems().map(([terms, defs], type) => {
+      const text = !defs.getBlocks().length ? `<text:line-break/><text:tab/>${defs.getText()}` : '';
+      const blocks = defs.getBlocks().length ? defs.getContent() : '';
+
+      return `<text:p text:style-name="TexteCourant">
+<text:span text:style-name="T7"><text:tab/>${terms.map(d => d.getText()).join(', ')}</text:span>${text}</text:p>${blocks}`;
+    }).join('');
+  },
+
+  inline_callout: ({node}) => {
+    return `<text:span text:style-name="PuceCode">${node.getText()}</text:span>`;
   },
 
   colist: ({node}) => {
-    return '';
+    const items = node.getItems().map((item, i) => {
+      return `<text:span text:style-name="PuceCode">${i+1}</text:span> ${item.getText()}<text:line-break/>`;
+    }).join('');
+
+    return `<text:p text:style-name="TexteCourant">${items}</text:p>`;
   },
 
   admonition: ({node}) => {
-    return `<text:h text:style-name="RemarqueTitre" text:outline-level="7">${node.getTitle()}</text:h>${node.getContent()}`;
+    let pre = '';
+
+    if (node.getTitle()) {
+      pre = `<text:h text:style-name="RemarqueTitre" text:outline-level="7">${node.getTitle()}</text:h>`;
+    }
+
+    return `${pre}${node.getContent()}`;
   },
 
   example: ({node}) => {
-    return '';
+    return node.getContent();
   },
 
   open: ({node}) => {
@@ -121,9 +149,21 @@ module.exports = {
 
   image: ({node}) => {
     const uri = node.getImageUri(node.getAttribute('target'));
+    const caption = node.getTitle() ? `<text:p text:style-name="RemarqueFigureLegende">${node.getTitle()}</text:p>` : '';
 
-    return `<text:p text:style-name="RemarqueFigureNumero">(${uri})</text:p>
-    <text:p text:style-name="RemarqueFigureLegende">${node.getTitle()}</text:p>`;
+    return `<text:p text:style-name="RemarqueFigureNumero">(${uri})</text:p>${caption}`;
+  },
+
+  preamble: ({node}) => {
+    const label = node.getDocument().getAttribute('toc-title');
+
+    return `<text:table-of-content text:style-name="Sect1" text:protected="true" text:name="Table des matières1">
+  <text:index-body>
+    <text:index-title text:style-name="Sect1" text:name="${label}_Head">
+      <text:p text:style-name="P12">${label}</text:p>
+    </text:index-title>
+  </text:index-body>
+</text:table-of-content>`;
   },
 
   table: ({node}) => {
